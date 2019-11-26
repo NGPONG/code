9eb257f00b7caea9550d303ddcf974ffa231a6ce
NGPONG <pengpeng1997428@gmail.com>
2019-11-26 23:29:46 +0800

    add asp.net mvc study project.

---
+++ ASP.NET MVC/ASP.NET MVC_Study_01/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASP.NET_MVC_Study_01.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult TestAPI(UserInfo users)
+        {
+            return Content($"Your Message is");
+        }
+
+        //[HttpPost]
+        //public ActionResult About()
+        //{
+        //    return base.Content($"Your Message is");
+        //}
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+
+    public class UserInfo
+    {
+        public int ID { get; set; }
+        public string UserName { get; set; }
+        public string UserPass { get; set; }
+        public System.DateTime RegTime { get; set; }
+        public string Email { get; set; }
+    }
+
+    public class RemoteInfo
+    {
+        public string MyMessage { get; set; }
+
+    }
+}
---
+++ ASP.NET MVC/ASP.NET MVC_Study_01/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ASP.NET_MVC_Study_01.MvcApplication" Language="C#" %>
