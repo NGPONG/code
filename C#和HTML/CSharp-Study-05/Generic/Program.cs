442e3b3953c5171968ba8985f11435f967463b70
NGPONG <pengpeng1997428@gmail.com>
2019-12-18 00:52:23 +0800

    [+] Add CSharp Study_05

---
+++ C#和HTML/CSharp-Study-05/Generic/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generic
+{
+    public delegate int GenericHandler<T>(T value1,T value2);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var commandString = new CommandMethod<string>();
+            var strResult = commandString.GetMaxLength(new string[] { "测试一下", "23333333","666" }, commandString.GetMaxString);
+
+            var commandInt = new CommandMethod<int>();
+            var intResult = commandInt.GetMaxLength(new int[] { 1, 2, 3, 4, 5, 6, 7, 8 }, commandInt.GetMaxInt);
+
+            Console.ReadLine();
+        }
+    }
+
+    public class CommandMethod<T>
+    {
+        public T GetMaxLength(T[] arrary,GenericHandler<T> handler)
+        {
+            T max = arrary[0];
+
+            for (int i = 0; i < arrary.Length; i++)
+            {
+                if (handler(arrary[i], max) > 0)
+                {
+                    max = arrary[i];
+                }
+            }
+
+            return max;
+        }
+
+        public int GetMaxInt(int value1, int value2)
+        {
+            return value1 - value2;
+        }
+
+        public int GetMaxString(string value1, string value2)
+        {
+            return value1.Length - value2.Length;
+        }
+    }
+}
