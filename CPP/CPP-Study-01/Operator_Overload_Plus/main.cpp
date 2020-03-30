3c7d312bcc94ecfa22ae2f6a795eab0f20941248
NGPONG <pengpeng1997428@gmail.com>
2020-03-30 14:11:53 +0800

    1

--- CPP/CPP-Study-01/Operator_Overload_Plus/main.cpp
+++ CPP/CPP-Study-01/Operator_Overload_Plus/main.cpp
@@ -12,9 +12,9 @@ public:
   };
   Person(const Person &per) {
     this->m_age = per.m_age;
-    this->m_name = new char[32];
+    this->m_name = new char[strlen(per.m_name) + 1];
     strcpy(this->m_name, per.m_name);
-  };
+  }
   ~Person() {
     printf("delete %p", this->m_name);
     delete[] this->m_name;
@@ -31,15 +31,21 @@ public:
   char *m_name;
 };
 
+/*
 Person operator+(Person &per_01, Person &per_02) {
   Person per_temp = Person(per_01.m_age + per_02.m_age, (char *)"DEFAULT");
   return per_temp;
 }
+*/
 
 int main(void) {
 
   Person per_01 = Person(4, (char *)"NGPONG");
   Person per_02 = Person(8, (char *)"Hello,World!");
+
+  
+
+  Person per_03 = per_01 + per_02;
 
   system("pause");
   return EXIT_SUCCESS;
--- CPP/CPP-Study-01/Operator_Overload_left-shift/main.cpp
+++ CPP/CPP-Study-01/Operator_Overload_left-shift/main.cpp
@@ -1,33 +1,43 @@
 #include <iostream>
 using namespace std;
 
+#include <string.h>
+
 class Person {
 
 public:
-  Person(int age, char *name) : m_age(age), m_name(name){};
+  Person(int age, char *name = (char *)"DEFAULT") { 
+    this->m_age = age;
+    this->m_name = NULL;
+
+    if(name != NULL) {
+      this->m_name = new char[strlen(name) + 1];
+      strcpy(this->m_name, name);
+    }
+  };
 
   Person(const Person &per) {
     this->m_age = per.m_age;
     this->m_name = per.m_name;
   }
+  ~Person() { 
+    cout << "Person destructor" << endl; 
+  }
 
+private:
   int m_age;
   char *m_name;
 };
 
-void operator<<(ostream *cout, Person &per) {
-  *cout << "Person Age = "
-        << per.m_age
-        << endl
-        << "Person Name = "
-        << per.m_name
-        << endl;
-}
+// ostream &operator<<(ostream &cout, Person *per) {
+//   cout 
+// }
 
 int main(void) {
 
-  Person per = Person(1024, (char *)"NGPONG!");
-  &cout << per;
+  Person per(1024, (char *)"Hello,World");
+  per = Person(256);
+
 
   system("pause");
   return EXIT_SUCCESS;
