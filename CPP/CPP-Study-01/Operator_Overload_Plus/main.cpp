ba488d7264242666948907e817b9e0f937908bb6
NGPONG <pengpeng1997428@gmail.com>
2020-04-05 01:53:30 +0800

    [*] study

--- CPP/CPP-Study-01/Operator_Overload_Plus/main.cpp
+++ CPP/CPP-Study-01/Operator_Overload_Plus/main.cpp
@@ -4,44 +4,37 @@ using namespace std;
 #include <string.h>
 
 class Person {
-  friend Person &operator+(Person &per, int num) {
-    per.m_age += num;
-    return per;
-  }
+  // friend Person &operator+(const Person &per, int num){
+  //   return (Person &)per;
+  // }
 
 public:
   Person(int age, char *name) {
     this->m_age = age;
-    this->m_name = new char[strlen(name) + 1];
-    strcpy(this->m_name, name);
   };
   Person(const Person &per) {
     this->m_age = per.m_age;
-    this->m_name = new char[strlen(per.m_name) + 1];
-    strcpy(this->m_name, per.m_name);
   }
   ~Person() {
-    printf("delete %p", this->m_name);
-    delete[] this->m_name;
+    cout << "Person destructor" << endl;
   }
 
 public:
   Person operator+(Person &per) {
-    Person per_temp = Person(per.m_age + this->m_age, (char *)"DEFAULT");
-    return per_temp;
+    this->m_age += per.m_age;
+    return *this;
   }
 
 public:
   int m_age;
-  char *m_name;
 };
 
 int main(void) {
 
   Person per_01 = Person(4, (char *)"NGPONG");
-  Person per_02 = Person(8, (char *)"Hello,World!");
+  Person per_02 = Person(8, (char *)"Hello,World");
 
-  Person &per_03 = (per_01 + per_02) + 10;
+  Person per = per_01 + per_02;
 
   system("pause");
   return EXIT_SUCCESS;
--- CPP/CPP-Study-01/TEST/TEST_08/main.cpp
+++ CPP/CPP-Study-01/TEST/TEST_08/main.cpp
@@ -4,8 +4,19 @@ using namespace std;
 
 int main(void) {
 
-  printf("Hello,World!\n");
 
-  system("pause");
+  int a = 5;                  /* a:左值, 5:右值 */
+  int b = a;                  /* b:左值, b:左值 */
+  int c = 5;                  /* c:左值, 5:右值 */
+  int d = a + 1;              /* d:左值, a + 1:右值 */
+  int *e = &a;                /* e:左值, &a:右值 */
+
+  //int &a_ref = a + 1;         /* a_ref:左值引用, a + 1:右值, 编译失败 */
+  const int &a_ref_c = a + 1; /* a_ref_c:常量左值引用, a + 1:右值, 编译通过 */
+
+  int &&a_r_r = a + 1;        /* a_r_r:右值引用, a + 1:右值 */
+  int &&a_r_r_c = move(a);    /* a_r_r_c:右值引用, move(a):将亡值 */
+
+
   return EXIT_SUCCESS;
 }
