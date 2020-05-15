5f8743f1ac89adba84ac72cc3f3de57521cb36e6
NGPONG <pengpeng1997428@gmail.com>
2020-05-16 03:03:26 +0800

    1

--- CPP/CPP-Study-01/STL_06/main.cpp
+++ CPP/CPP-Study-01/STL_06/main.cpp
@@ -10,7 +10,9 @@ void foo(void) {
 
   sys.print("Please choose your favorite hero:\n");
   sys.get_heros();
+  sys.select_heros();
 
+  sys.print("Please choose your what weapon will used\n");
   
 }
 
---
+++ CPP/CPP-Study-01/STL_06/player.h
@@ -0,0 +1,12 @@
+#ifndef __PLAYER_H__
+#define __PLAYER_H__
+
+#include "hero.h"
+
+class Player {
+public:
+  hero &selected_hero;
+};
+
+
+#endif /* end of include guard: __PLAYER_H__ */
