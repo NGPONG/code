4ffe41f0d6902027c922a718790be806e0e9d70a
NGPONG <pengpeng1997428@gmail.com>
2020-05-19 01:08:17 +0800

    1

--- QT/HELLO_QT/mywidget.cpp
+++ QT/HELLO_QT/mywidget.cpp
@@ -3,11 +3,13 @@
 MyWidget::MyWidget(QWidget *parent)
   : QWidget(parent)
 {
-//  QPushButton *button = new QPushButton("Hello",this);
-//  QPushButton *button = new QPushButton();
-//  button->setParent(this);
+  //  QPushButton *button = new QPushButton("Hello",this);
+  //  QPushButton *button = new QPushButton();
+  //  button->setParent(this);
 
-
+  QPushButton button;
+  button.setParent(this);
+  button.show();
 }
 
 MyWidget::~MyWidget()
