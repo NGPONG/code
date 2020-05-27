d83de02747eba64550fecc1d5af6f8dffaffe3ec
NGPONG <pengpeng1997428@gmail.com>
2020-05-27 21:27:52 +0800

    1

--- QT/CoinFilp/buttonstart.cpp
+++ QT/CoinFilp/buttonstart.cpp
@@ -5,21 +5,21 @@ ButtonStart::ButtonStart(QWidget *_par, QString _normal, QString _press)
   QPixmap pixmap;
   bool ret = pixmap.load(normal_img_path);
   if (!ret) {
-      qDebug() << normal_img_path << "加载图片失败!";
+    qDebug() << normal_img_path << "加载图片失败!";
   }
 
-  this->setFixedSize( pixmap.width(), pixmap.height() );
+  this->setFixedSize(pixmap.width(), pixmap.height());
   this->setStyleSheet("QPushButton{border:0px;}");
   this->setIcon(pixmap);
-  this->setIconSize(QSize(pixmap.width(),pixmap.height()));
+  this->setIconSize(QSize(pixmap.width(), pixmap.height()));
 }
 
 void ButtonStart::zoom_down(void) {
   /* Create a new animation instance */
-  QPropertyAnimation *ani = new QPropertyAnimation(this,"geometry");
+  QPropertyAnimation *ani = new QPropertyAnimation(this, "geometry");
   ani->setDuration(200);
-  ani->setStartValue(QRect(this->x(),this->y() + 10,this->width(),this->height()));
-  ani->setEndValue(QRect(this->x(),this->y(),this->width(),this->height()));
+  ani->setStartValue(QRect(this->x(), this->y() + 10, this->width(), this->height()));
+  ani->setEndValue(QRect(this->x(), this->y(), this->width(), this->height()));
   /* Set the transition curve, QEasingCurve::OutBounce is the bounce effect */
   ani->setEasingCurve(QEasingCurve::OutBounce);
   /* start */
--- QT/CoinFilp/buttonstart.h
+++ QT/CoinFilp/buttonstart.h
@@ -1,23 +1,22 @@
 #ifndef BUTTONSTART_H
 #define BUTTONSTART_H
 
-#include <QPushButton>
-#include <QPixmap>
 #include <QDebug>
+#include <QPixmap>
 #include <QPropertyAnimation>
+#include <QPushButton>
 
-class ButtonStart : public QPushButton
-{
+class ButtonStart : public QPushButton {
   Q_OBJECT
-public:
-  explicit ButtonStart(QWidget *_par, QString _normal, QString _press = "");
+  public:
+    explicit ButtonStart(QWidget* _par, QString _normal, QString _press = "");
 
-private:
-  QString normal_img_path;  /* Display picture path by default */
-  QString pressed_Img_path; /* Press to display the picture path */
+  private:
+    QString normal_img_path;  /* Display picture path by default */
+    QString pressed_Img_path; /* Press to display the picture path */
 
-public:
-  void zoom_down(void);
+  public:
+    void zoom_down(void);
 };
 
-#endif // BUTTONSTART_H
+#endif  // BUTTONSTART_H
