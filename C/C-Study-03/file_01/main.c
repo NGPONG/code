d15fdbe6ef04fd36d955e325149a00c60839f661
NGPONG <pengpeng1997428@gmail.com>
2020-06-17 08:34:33 +0800

    1

---
+++ C/C-Study-03/file_01/main.c
@@ -0,0 +1,11 @@
+#include <math.h>
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+int main(void) {
+  printf("hello");
+  return EXIT_SUCCESS;
+}
