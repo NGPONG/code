1815d1a18160f03df5544c742be36aaa98b7303d
NGPONG <pengpeng1997428@gmail.com>
2020-06-17 20:18:53 +0800

    1

--- C/C-Study-03/file_01/main.c
+++ C/C-Study-03/file_01/main.c
@@ -6,24 +6,28 @@
 #include <stdbool.h>
 
 int main(void) {
-  FILE *f_read = fopen("./text", "w");
+  FILE *f_read = fopen("/home/ngpong/code/C/C-Study-03/file_01/test.txt", "r");
   if (f_read == NULL) {
-    perror("open file error");
+    perror("E");
+    return EXIT_FAILURE;
   }
+  printf("fp = %p\n", f_read);
 
   char *str = malloc(sizeof(char) * 0x400);
   if (str == NULL) {
     perror("heap error!");
   }
+  printf("str = %p\n", str);
 
-  while (feof(f_read)) {
-    fgets(str, 0x400, f_read);
-    printf("%s\n", str);
-
+  while (!feof(f_read)) {
+    fgets(str, sizeof(char) * 0x400, f_read);
+    printf("%s", str);
     memset(str, 0x0, sizeof(char) * 0x400);
   }
 
   fclose(f_read);
+  printf("free %p\n", f_read);
   free(str);
+  printf("free %p\n", str);
   return EXIT_SUCCESS;
 }
---
+++ C/C-Study-03/file_02/main.c
@@ -0,0 +1,15 @@
+#include <math.h>
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+int main(void) {
+
+
+  return EXIT_SUCCESS;
+}
