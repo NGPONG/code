9cab794b31554d641b57a506c31cf4a0acd57cdf
NGPONG <pengpeng1997428@gmail.com>
2020-06-17 08:56:46 +0800

    1

--- C/C-Study-03/file_01/main.c
+++ C/C-Study-03/file_01/main.c
@@ -6,6 +6,24 @@
 #include <stdbool.h>
 
 int main(void) {
-  printf("hello");
+  FILE *f_read = fopen("./text", "w");
+  if (f_read == NULL) {
+    perror("open file error");
+  }
+
+  char *str = malloc(sizeof(char) * 0x400);
+  if (str == NULL) {
+    perror("heap error!");
+  }
+
+  while (feof(f_read)) {
+    fgets(str, 0x400, f_read);
+    printf("%s\n", str);
+
+    memset(str, 0x0, sizeof(char) * 0x400);
+  }
+
+  fclose(f_read);
+  free(str);
   return EXIT_SUCCESS;
 }
