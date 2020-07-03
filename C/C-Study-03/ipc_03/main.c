1c0c2c34d7b4be2e4cb2964aa706ea79154f8cb8
NGPONG <pengpeng1997428@gmail.com>
2020-07-03 18:36:23 +0800

    1

--- C/C-Study-03/ipc_03/main.c
+++ C/C-Study-03/ipc_03/main.c
@@ -7,7 +7,7 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-void foo(void) {
+void foo_01(void) {
   int fd = open("./myfifo.p", O_RDONLY);
   if (fd < 0) {
     perror("e");
@@ -16,13 +16,62 @@ void foo(void) {
 
   char buf[0x40] = { 0 };
   int size = read(fd, buf, sizeof(buf));
-  printf("size = %d, %s", size, buf);
+  printf("size = %d, %s\n", size, buf);
 
   close(fd);
 }
 
+void foo_02(void) {
+  int fd_r = open("./myfifo.p", O_RDONLY);
+  if (fd_r < 0) {
+    perror("e");
+    exit(EXIT_FAILURE);
+  }
+
+  /* int fd_w = open("./myfifo.p", O_WRONLY); */
+  /* if (fd_w < 0) {                          */
+  /*   perror("e");                           */
+  /*   exit(EXIT_FAILURE);                    */
+  /* }                                        */
+
+  printf("123123");
+
+  int pid = fork();
+  if (pid < 0) {
+    perror("e");
+    exit(EXIT_FAILURE);
+  } else if (pid == 0) { /* child process context */
+    printf("child start");
+
+    /* int idx = 0;                      */
+    /* while (true) {                    */
+    /*   ++idx;                          */
+    /*   write(fd_w, &idx, sizeof(int)); */
+
+    /*   sleep(1);                       */
+    /* }                                 */
+
+    exit(EXIT_SUCCESS);
+  }
+
+  printf("parent start");
+
+  /* parent process context */
+  char buf[64] = { 0 };
+
+  /* while (true) {                   */
+  /*   read(fd_r, buf, sizeof(buf));  */
+  /*   printf("%s", buf);             */
+
+  /*   memset(buf, 0x0, sizeof(buf)); */
+  /* }                                */
+  
+  exit(EXIT_SUCCESS);
+}
+
 int main(int argc, char *argv[]) {
-  foo();
+  /* foo_01(); */
+  foo_02();
 
   return EXIT_SUCCESS;
 }
