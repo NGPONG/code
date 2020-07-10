2300d574545cd4035850620adedffbbbad513ab1
吴鹏 <pengpeng1997428@gmail.com>
2020-07-10 16:24:02 +0800

    1

--- c/c_study_03/process_08/main.c
+++ c/c_study_03/process_08/main.c
@@ -4,8 +4,26 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
 int main(int argc, char *argv[]) {
-  
+  int pid = fork();
+  if (pid == 0) {
+    int idx = 0;
+    while ((++idx) < 0x400) {
+      printf("child executeing %d\n", idx);
+      sleep(1);
+    }
+
+    exit(EXIT_SUCCESS);
+  }
+
+  int idx = 0;
+  while ((++idx) < 5) {
+    printf("parent executeing %d\n",idx);
+    sleep(1);
+  }
+
   return EXIT_SUCCESS;
 }
