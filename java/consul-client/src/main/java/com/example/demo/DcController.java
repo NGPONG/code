b43d9d6d98ce12b23fff59683aaa7c93f77ae498
ngpong <pengpeng1997428@gmail.com>
2020-10-22 17:33:13 +0800

    1

---
+++ java/consul-client/src/main/java/com/example/demo/DcController.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DcController {
+  final Logger logger = LoggerFactory.getLogger(getClass());
+
+  @Autowired
+  DiscoveryClient discoveryClient;
+
+  @GetMapping("/dc")
+  public String dc() {
+    String services = "Services: " + discoveryClient.getServices();
+    logger.info(services);
+    return services;
+  }
+}
