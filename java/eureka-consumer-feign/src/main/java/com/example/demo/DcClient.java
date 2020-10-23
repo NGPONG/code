40d50b325c3f1e3db2d4af1c3d1a14d1254f4a5e
ngpong <pengpeng1997428@gmail.com>
2020-10-23 17:31:02 +0800

    1

---
+++ java/eureka-consumer-feign/src/main/java/com/example/demo/DcClient.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@FeignClient("eureka-client")
+public interface DcClient {
+  @GetMapping("/dc")
+  String consumer();
+}
