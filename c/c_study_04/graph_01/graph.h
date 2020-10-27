f9bf450bdc8d21ec529eb081c3b6d55cfac656db
NGPONG <pengpeng1997428@gmail.com>
2020-10-28 01:45:18 +0800

    1

--- c/c_study_04/graph_01/graph.h
+++ c/c_study_04/graph_01/graph.h
@@ -8,14 +8,15 @@ typedef char vertex_type;   /* Type of vertex, it's might should be defined by u
 typedef int edge_type;      /* Type of weight, it's might should be defined by users */
 
 typedef struct {
-  vertex_type vexs[5]; /* Table of vertex */
-  edge_type arc[5][5]; /* Adjacency Matrix, can be seen as table of edge */
+  vertex_type vexs[9]; /* Table of vertex */
+  edge_type arc[9][9]; /* Adjacency Matrix, can be seen as table of edge */
   int vertexs_num, edges_num;
 } n_graph;
 
-void init_graph(n_graph *G);
-void create_edge_node_by_pait(int i, int j, edge_type (*arc)[5]);
+void init_graph(n_graph *G, int _vertexs_num, int _edges_num);
+void create_edge_node_by_pait(int i, int j, edge_type weight, edge_type (*arc)[9]);
 void DES_traverse(n_graph *G);
 void BFS_traverse(n_graph *G);
+void min_span_tree_prim(n_graph *_G);
 
 #endif /* end of include guard: __GRAPH_H__ */
--- c/c_study_04/graph_01/main.c
+++ c/c_study_04/graph_01/main.c
@@ -7,19 +7,50 @@
 
 #include "graph.h"
 
+/* @breif 依照书中的 图7-6-3 进行初始化 */
 int main(void) {
   n_graph *G = (n_graph *)malloc(sizeof(n_graph));
-  init_graph(G);
+  init_graph(G, 9, 15);
 
-  create_edge_node_by_pait(0, 4, G->arc);
-  create_edge_node_by_pait(1, 0, G->arc);
-  create_edge_node_by_pait(1, 2, G->arc);
-  create_edge_node_by_pait(1, 0, G->arc);
-  create_edge_node_by_pait(2, 0, G->arc);
-  create_edge_node_by_pait(2, 3, G->arc);
-  create_edge_node_by_pait(3, 4, G->arc);
+  create_edge_node_by_pait(0, 1, 10, G->arc);
+  create_edge_node_by_pait(0, 5, 11, G->arc);
+
+  create_edge_node_by_pait(1, 0, 10, G->arc);
+  create_edge_node_by_pait(1, 2, 18, G->arc);
+  create_edge_node_by_pait(1, 6, 16, G->arc);
+  create_edge_node_by_pait(1, 8, 12, G->arc);
+
+  create_edge_node_by_pait(2, 3, 22, G->arc);
+  create_edge_node_by_pait(2, 8, 8, G->arc);
+
+  create_edge_node_by_pait(3, 2, 22, G->arc);
+  create_edge_node_by_pait(3, 4, 20, G->arc);
+  create_edge_node_by_pait(3, 7, 16, G->arc);
+  create_edge_node_by_pait(3, 8, 21, G->arc);
+
+  create_edge_node_by_pait(4, 3, 20, G->arc);
+  create_edge_node_by_pait(4, 5, 26, G->arc);
+  create_edge_node_by_pait(4, 7, 7, G->arc);
+
+  create_edge_node_by_pait(5, 0, 11, G->arc);
+  create_edge_node_by_pait(5, 4, 26, G->arc);
+  create_edge_node_by_pait(5, 6, 17, G->arc);
+
+  create_edge_node_by_pait(6, 1, 16, G->arc);
+  create_edge_node_by_pait(6, 5, 17, G->arc);
+  create_edge_node_by_pait(6, 7, 19, G->arc);
+
+  create_edge_node_by_pait(7, 3, 16, G->arc);
+  create_edge_node_by_pait(7, 4, 7, G->arc);
+  create_edge_node_by_pait(7, 6, 19, G->arc);
+  
+  create_edge_node_by_pait(8, 1, 12, G->arc);
+  create_edge_node_by_pait(8, 2, 8, G->arc);
+  create_edge_node_by_pait(8, 3, 21, G->arc);
 
   /* DES_traverse(G); */
+
+  min_span_tree_prim(G);
 
   BFS_traverse(G);
 
