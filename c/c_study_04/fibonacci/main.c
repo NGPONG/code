6ddc86a8cfa5fec31e457f7733c92e849a8cc1cc
NGPONG <pengpeng1997428@gmail.com>
2020-11-06 01:23:08 +0800

    1

--- c/c_study_04/fibonacci/main.c
+++ c/c_study_04/fibonacci/main.c
@@ -1,6 +1,3 @@
-#pragma warning(disable:6031)
-#pragma warning(disable:4996)			
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
@@ -9,16 +6,17 @@
 #include <time.h>
 #include <Windows.h>
 
-int fibonacci(int postion) {
-	if (postion == 1 || postion == 2) {
-		return 1;
-	}
+int fibonacci(unsigned int n) {
+	if(n < 2) return n == 0 ? 0 : 1;
 
-	return fibonacci(postion - 1) + fibonacci(postion - 2);
+  return fibonacci(n - 1) + fibonacci(n - 2);
 }
 
 int main(void) {
-	int i = fibonacci(10);
+	/* 打印前斐波那契数列中的前 40 个数 */
+	for (size_t i = 0; i < 40; ++i) {
+    printf("%d(%d)\n", fibonacci(i), i);
+  }
 
 	system("pause");
 	return EXIT_SUCCESS;
