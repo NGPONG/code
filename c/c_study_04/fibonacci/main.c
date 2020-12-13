0c83173456cdb816775390806a1bc48b2a70a01b
ngpong <pengpeng1997428@gmail.com>
2020-12-13 15:21:37 +0800

    1

--- c/c_study_04/fibonacci/main.c
+++ c/c_study_04/fibonacci/main.c
@@ -7,17 +7,17 @@
 #include <Windows.h>
 
 int fibonacci(unsigned int n) {
-	if(n < 2) return n == 0 ? 0 : 1;
+  if (n < 2) return n == 0 ? 0 : 1;
 
   return fibonacci(n - 1) + fibonacci(n - 2);
 }
 
 int main(void) {
-	/* 打印前斐波那契数列中的前 40 个数 */
-	for (size_t i = 0; i < 100000; ++i) {
+  /* 打印前斐波那契数列中的前 40 个数 */
+  for (size_t i = 0; i < 100000; ++i) {
     printf("%d(%d)\n", fibonacci(i), i);
   }
 
-	system("pause");
-	return EXIT_SUCCESS;
+  system("pause");
+  return EXIT_SUCCESS;
 }
