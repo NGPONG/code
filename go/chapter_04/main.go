d19e4310fed6b4bdfe8b69d47a6cc224741002d3
ngpong <pengpeng1997428@gmail.com>
2020-12-24 01:28:35 +0800

    1

--- go/chapter_04/main.go
+++ go/chapter_04/main.go
@@ -1,16 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-func reverse(s *[6]int) {
-	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
-		s[i], s[j] = s[j], s[i]
-	}
+func appendInt(src []int, val int) []int {
+  var tmp []int
+  zlen := len(src) + 1
+  if zlen <= cap(src) {
+    tmp = src[:zlen]
+  } else {
+    zcap := zlen
+    if zcap < 2 * len(src) {
+      zcap = 2 * len(src)
+    }
+    tmp = make([]int, zlen, zcap)
+    copy(tmp, src)
+  }
+  tmp[len(src)] = val
+
+  fmt.Printf("len = %d, cap = %d ", len(tmp), cap(tmp))
+
+  return tmp
+}
+
+func non_empty(src []string) []string {
+  out := src[:0]
+  for _, s := range src {
+    if (s != "") {
+      out = append(out, s)
+    }
+  }
+  return out
 }
 
 func main() {
-	a := [...]int{0, 1, 2, 3, 4, 5}
-	reverse(&a)
-
-	fmt.Println(a)
+  data := []string{"hello", "", "world", ""}
+  fmt.Println(data)
+  new_data := non_empty(data)
+  fmt.Println(new_data)
 }
