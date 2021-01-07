e29fd1960f2f0692055fcc80d9114137bca8470e
NGPONG <pengpeng1997428@gmail.com>
2021-01-08 01:38:23 +0800

    1

--- cpp/CPP-Study-02/TEST/TEST_03/main.cpp
+++ cpp/CPP-Study-02/TEST/TEST_03/main.cpp
@@ -1,14 +1,20 @@
 #include <iostream>
-#include <map>
-#include <string>
+#include <vector>
+#include <algorithm>
 
+using namespace std;
+
+bool f_sort(int &_left,int &_right) {
+  return _left > _right;
+}
+void foo(void) {
+  int nums[5] = { 3, 2, 4, 1, 5 };
+  vector<int> v(nums, nums + (sizeof(nums) / sizeof(int)));
+
+  sort(v.begin(), v.end(), ptr_fun(::f_sort));
+}
 int main(void) {
-  using namespace std;
-
-  map<int, string> m;
-  m[0] = "A";
-  m[1] = "B";
-
+  foo()
 
   return EXIT_SUCCESS;
 }
