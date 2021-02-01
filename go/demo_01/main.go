449220ead3bbea960c4eaf2b3cf5bd3c45401d83
NGPONG <v_wupengwu@tencent.com>
2021-02-01 18:04:27 +0800

    1

--- go/demo_01/main.go
+++ go/demo_01/main.go
@@ -6,38 +6,35 @@ import (
 	"fmt"
 )
 
-type User struct {
-    ID       int64  `gorm:"column:id;primary_key"` 
-    Username string `gorm:"column:username"`
-    Password string `gorm:"column:password"`
-	Test_01  string `gorm:"column:test_01"`
+type ActivityResponseListItem struct {
+	ID			  uint	 `gorm:"column:id" json:"id"`						  // 序号
+	ActivityName  string `gorm:"column:activity_name" json:"activity_name"`   // 活动奖励名
+	RewardRule    string `gorm:"column:reward_rule" json:"reward_rule"`       // AMS单号
+	ReceivedNum   string `gorm:"column:received_num" json:"received_num"`     // 总发放数量/实际发放数量
+	Operators     string `gorm:"column:operators" json:"operators"`           // 最后操作人
+	RewardAddress string `gorm:"column:reward_address" json:"reward_address"` // 奖励链接
+	ActRewardNum  int	 `gorm:"column:act_reward_num" json:"act_reward_num"` // 实际发放数量
+	RewardNum	  int    `gorm:"column:reward_num" json:"reward_num"`		  // 总发放数量
 }
-
-func (User) TableName() string {
-    return "users"
-}
-
-const (
-	page_size = 10
-	page_idx  = 1
-)
 
 func main() {
-    db, err := gorm.Open("mysql", "root:Pengqi1997.0428@tcp(192.168.255.10:3306)/ngpong_test?charset=utf8")
+    db, err := gorm.Open("mysql", "root:Pengqi1997.0428@tcp(192.168.255.10:3306)/ingame_qywechat_activity?charset=utf8")
 	db.LogMode(true)
 	defer db.Close()
     if err != nil {
         fmt.Printf("mysql connect error %v", err)
     }
 
-	var ret []User
+	var ret []ActivityResponseListItem
 
-	query := db.Table("users, count(1) as num")
-	query = query.Group("username")
-	query = query.Where("username in (?)", "ngpong")
-	query = query.Select("username, password, test_01")
+	query := db.Table("ingame_qywechat_activity.activity")
+	query = query.Joins("left join ingame_qywechat_activity.user_reward on ingame_qywechat_activity.user_reward.activity_id = ingame_qywechat_activity.activity.id")
+	query = query.Select("ingame_qywechat_activity.activity.id, activity_name, reward_address, reward_rule, reward_num, ingame_qywechat_activity.activity.updator as operators, concat(count(ingame_qywechat_activity.user_reward.id), ' / ', reward_num) as received_num, count(ingame_qywechat_activity.user_reward.id) as act_reward_num")
+	query = query.Group("ingame_qywechat_activity.activity.id")
+	query = query.Where("game_id = ?", "lol")
+	query = query.Order("ingame_qywechat_activity.activity.created desc")
 
-	query.Find(&ret)
+	query.Offset(1).Limit(1).Find(&ret)
 
 	fmt.Println(ret)
 }
