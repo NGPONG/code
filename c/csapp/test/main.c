516c86477a51ab4d7f720b6dc21a88f5049d4f57
ngpong <pengpeng1997428@gmail.com>
2021-02-16 21:24:47 +0800

    1

--- c/csapp/test/main.c
+++ c/csapp/test/main.c
@@ -1,9 +1,10 @@
-#include <math.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <complex.h>
 
-float my_foo(float a, float b) { return fmax(a, b); }
-
-int main() {
-  printf("%f\n", my_foo(4.5, 3.1));
-  return 0;
+int main(int argc, char *argv[]) {
+  
+  return EXIT_SUCCESS;
 }
