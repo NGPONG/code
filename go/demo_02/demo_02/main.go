ae89fa2895f08ff78425b9f2d375560626cb1889
NGPONG <pengpeng1997428@gmail.com>
2021-03-03 18:27:34 +0800

    1

--- go/demo_02/demo_02/main.go
+++ go/demo_02/demo_02/main.go
@@ -2,16 +2,13 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
 func main() {
-	dt, err := time.Parse("2006-01-02 15:04", "2018-01-20 04:35")
-	if err != nil {
-		fmt.Println(err)
+	built := interface{
+		game: "hello"
 	}
-	
 
-	fmt.Println(dt)
+	
 }
 
