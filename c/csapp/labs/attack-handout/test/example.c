12151fc93191064fdd6e8f35373a5a7814f3cfd0
ngpong <pengpeng1997428@gmail.com>
2021-03-29 00:53:24 +0800

    1

---
+++ c/csapp/labs/attack-handout/test/example.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+int main(int argc, char *argv[]) {
+  unsigned x = 1024;
+  printf("%.8x", x);
+
+  return EXIT_SUCCESS;
+}
---
+++ c/csapp/labs/attack-handout/test/example.s
@@ -0,0 +1,3 @@
+pushq $0x4017ec # set postion of address about <touch2>
+movl $0x59b997fa, %edi
+retq
