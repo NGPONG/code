21f1bd67b4062fcc3469664eaf7d68686aa296c0
NGPONG <pengpeng1997428@gmail.com>
2021-03-29 18:21:21 +0800

    1

--- go/demo_03/main.go
+++ go/demo_03/main.go
@@ -1,26 +1,36 @@
 package main
 
 import (
-	"crypto/sha1"
-	"encoding/hex"
+	"encoding/json"
 	"fmt"
-	"io"
 )
 
+
+type MyStruct struct {
+	Id int `json:"id"`
+	Name string `json:"name"`
+}
+
+
 func main() {
-	str := "www.5lmh.com"
+	var str string = "[{\"id\":1041,\"name\":\"鸿图之友\"}, {\"id\":2,\"name\":\"\"}]";
 
-	//方法一    
 	data := []byte(str)
-	has := sha1.Sum(data)
-	shastr1 := fmt.Sprintf("%x", has) //将[]byte转成16进制    
-	fmt.Println(shastr1)
+	
+	ret := []struct{
+		Id int;
+		Name string
+	}{}
 
-	//方法二    
-	w := sha1.New()
-	io.WriteString(w, str) //将str写入到w中    
-	bw := w.Sum(nil) //w.Sum(nil)将w的hash转成[]byte格式    
-	// shastr2 := fmt.Sprintf("%x", bw)    //将 bw 转成字符串    
-	shastr2 := hex.EncodeToString(bw) //将 bw 转成字符串    
-	fmt.Println(shastr2)
+	// ret := []MyStruct{}
+
+	// var ret []map[string]interface{}
+
+	err := json.Unmarshal(data, &ret)
+	if err != nil {
+		fmt.Printf("error %v\n", err)
+	}
+
+	fmt.Printf("len = %d\n", len(ret))
+	fmt.Printf("%v\n", ret)
 }
