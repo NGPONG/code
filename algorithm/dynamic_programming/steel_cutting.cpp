7c4d23fdfe33a6981fb593689fec85465185eb43
ngpong <pengpeng1997428@gmail.com>
2021-06-04 01:04:17 +0800

    1

--- algorithm/dynamic_programming/steel_cutting.cpp
+++ algorithm/dynamic_programming/steel_cutting.cpp
@@ -28,7 +28,7 @@ int cut_steel_2_impl(int length, std::vector<int> &prices, std::vector<int> &dp)
   if (length == 0) {
     return 0;
   }
-  if (dp[length] != INT_MAX) {
+  if (dp[length] != -1) {
     return dp[length];
   }
 
@@ -37,18 +37,13 @@ int cut_steel_2_impl(int length, std::vector<int> &prices, std::vector<int> &dp)
     opt = std::max(opt, prices[i] + cut_steel_2_impl(length - i, prices, dp));
   }
 
-  if (opt == -1) {
-    printf("detected opt = -1, length: %d\n", length);
-    fflush(stdout);
-  }
-
   dp[length] = opt;
   return opt;
 }
 void cut_steel_2(std::vector<int> &prices) {
-  std::vector<int> dp(27, INT_MAX);
+  std::vector<int> dp(11, -1);
 
-  for (int i = 1; i <= 27; ++i) {
+  for (int i = 1; i <= 10; ++i) {
     int opt = cut_steel_2_impl(i, prices, dp);
     printf("尺寸为%d的最大收益为%d\n", i, opt);
   }
@@ -58,7 +53,7 @@ void cut_steel_2(std::vector<int> &prices) {
 int main(void) {
   std::vector<int> prices = { 0, 1, 5, 8, 9, 10, 17, 17, 20, 24, 30 };
   cut_steel_1(prices);
-  // cut_steel_2(prices);
+  cut_steel_2(prices);
 
   return 0;
 }
