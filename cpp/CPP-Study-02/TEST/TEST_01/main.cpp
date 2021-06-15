f3a5e7cc0a47fcdc36f2347d3aaaefe473a4f9ba
ngpong <pengpeng1997428@gmail.com>
2021-06-15 22:48:54 +0800

    1

--- cpp/CPP-Study-02/TEST/TEST_01/main.cpp
+++ cpp/CPP-Study-02/TEST/TEST_01/main.cpp
@@ -1,46 +1,12 @@
 #include <iostream>
+#include <map>
+#include <string>
+
 using namespace std;
 
-#include <windows.h>
-#include <functional>
-
-function<void(void)> __fn;
-
-void init(void) {
-  int i = 1;
-  __fn = [=]() mutable {
-    cout << ++i << endl;
-  };
-}
-
-void foo(void) {
-  while (true) {
-    __fn();
-    Sleep(1000);
-  }
-}
-
 int main(void) {
-  /* init();
-  foo(); */
-
-  int i = 10;
-  printf("%p\n", &i);
-
-  int *p = &i;
-
-  function<void(void)> _fn = [=]() mutable {
-    cout << ((*p) += 1) << " ";
-  };
-
-  _fn();
-  _fn();
-  _fn();
-
-  cout << i << endl;
-
-  cout << sizeof(_fn) << endl;
-
-  system("pause");
+  map<string, int> m;
+  auto p = m.insert(pair("a", 1));
+  
   return EXIT_SUCCESS;
 }
