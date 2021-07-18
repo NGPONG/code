a69d22d7f0bb9fe299227fc1d7e70b36bb5871b9
ngpong <pengpeng1997428@gmail.com>
2021-07-18 22:06:24 +0800

    1

--- cpp/CPP-Study-02/TEST/TEST_05/main.cpp
+++ cpp/CPP-Study-02/TEST/TEST_05/main.cpp
@@ -6,31 +6,32 @@ class BASE {
 public:
   virtual void foo() {
     std::cout << "BASE foo" << std::endl;
+    std::cout << *(int *)((char *)this + 8) << std::endl;
   }
 
-private:
-  ~BASE() {
+  virtual ~BASE() {
     foo();
     std::cout << "BASE destructor" << std::endl;
   }
 };
 
-// class CHILD : public BASE {
-// private:
-//   void foo() override {
-//     std::cout << "CHILD foo" << std::endl;
-//   }
+class CHILD : public BASE {
+int var = 1024;
 
-//   ~CHILD() override {
-//     std::cout << "CHILD destructor" << std::endl;
-//   }
-// };
+public:
+  void foo() override {
+    std::cout << "CHILD foo" << std::endl;
+  }
+
+  ~CHILD() override {
+    std::cout << "CHILD destructor" << std::endl;
+  }
+};
 
 int main(void) {
-  // BASE *b = new CHILD();
-  // b->foo();
-  // delete b;
-  BASE *b = new BASE;
+  BASE *b = new CHILD();
+  b->foo();
+
   delete b;
 
   return EXIT_SUCCESS;
