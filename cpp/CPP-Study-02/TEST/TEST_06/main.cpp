01939efd8a90667bafde8dbf085e0aa6de49eb00
ngpong <pengpeng1997428@gmail.com>
2021-07-27 21:57:02 +0800

    1

--- cpp/CPP-Study-02/TEST/TEST_06/main.cpp
+++ cpp/CPP-Study-02/TEST/TEST_06/main.cpp
@@ -1,11 +1,55 @@
 #include <iostream>
 #include <exception>
 
-void foo(void) {
+class base {
+public:
+  int num;
+
+  base(int _num) {
+    this->num = _num;
+    std::cout << "constructor" << std::endl;
+  }
+
+  base(const base &b) {
+    this->num = b.num;
+    std::cout << "copy constructor" << std::endl;
+  }
+
+  base(base &&b) {
+    this->num = b.num;
+    std::cout << "move constructor" << std::endl;
+  }
+
+  ~base() {
+    std::cout << "destructor" << std::endl;
+  }
+};
+
+base foo(void) {
+  int num = 0;
+  for (int i = 0; i < 10; ++i) {
+    num += i;
+  }
+
+  num *= 10;
+
+  base b(num);
+  std::cout << &b << std::endl;
+
+  return b;
+}
+
+void test() {
+  base b = foo();
+  std::cout << &b << std::endl;
+
+  for (int i = 0; i < 10; ++i) {
+    std::cout << b.num << std::endl;
+  }
 }
 
 int main(void) {
-  foo();
-  
+  test();
+
   return EXIT_SUCCESS;
 }
