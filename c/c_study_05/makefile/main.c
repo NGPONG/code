7a172633eca6e305f6faca77c245f4dd67a131de
ngpong <pengpeng1997428@gmail.com>
2021-08-15 01:37:46 +0800

    1

--- c/c_study_05/makefile/main.c
+++ c/c_study_05/makefile/main.c
@@ -7,6 +7,6 @@
 
 int main(int argc, char *argv[]) {
   foo();
-  
+
   return EXIT_SUCCESS;
 }
--- c/c_study_05/test_02/main.c
+++ c/c_study_05/test_02/main.c
@@ -1,20 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <stdbool.h>
+#include <unistd.h>
 
-void fun_1() {
-  int i = 10;
-  i += 1024;
+#define ZBX_UNUSED(var) (void)(var)
 
-  int *p = NULL;
-  *p = i;
-
-  printf("%d\n", *p);
+void foo(int i, int b) {
+  (void)(i);
+  (void)(b);
 }
 
 int main(int argc, char *argv[]) {
-  fun_1();
-  
+
   return EXIT_SUCCESS;
 }
