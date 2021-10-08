d064081e257cc0ab7cb2e031d980e79f552135fb
ngpong <pengpeng1997428@gmail.com>
2021-10-09 00:13:44 +0800

    1

--- cpp/CPP-Study-02/TEST/TEST_09/main.cpp
+++ cpp/CPP-Study-02/TEST/TEST_09/main.cpp
@@ -3,45 +3,15 @@
 #include <deque>
 #include <functional>
 
-template<typename F>
-class KWQueueUpLogScopeExit 
-{
-public:
-    KWQueueUpLogScopeExit(const F& f) : func(f) {}
-    ~KWQueueUpLogScopeExit() {
-        func();
-    }
-
-private:
-    F func;
-};
-
-template<typename S>
-class BASE 
-{
-public:
-  BASE(S str): mStr(str) {
-    std::cout << "hello,world" << std::endl;
-  }
-
-  BASE(std::string&& str): mStr(str) {
-    std::cout << "hello,world" << std::endl;
-  }
-
-  ~BASE() {
-    std::cout << "NGPONG" << std::endl;
-  }
-
-private:
-  S mStr;
-};
+int *foo(void) {
+    int i = 10;
+    return &i;
+}
 
 int main(void) {
-  KWQueueUpLogScopeExit ([&]() {
-    std::cout << "hello,world!" << std::endl;
-  });
+    int *i = foo();
 
-  std::cout << "NGPOPNG"  << std::endl;
+    *i = 256;
 
   return -1;
 }
