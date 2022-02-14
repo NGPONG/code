602e590b3e2356126c1c3f9a6e3aefb4ddf32e33
wupeng <wupeng122041@pwrd.com>
2022-02-14 11:24:19 +0800

    1

--- cpp/CPP-Study-02/TEST/TEST_19/main.cpp
+++ cpp/CPP-Study-02/TEST/TEST_19/main.cpp
@@ -1,19 +1,89 @@
 #include <iostream>
 #include <string>
-#include <sstream>
-#include <iomanip>
+#include <set>
+
+// #include <benchmark/benchmark.h>
+
+class BASE {
+public:
+  std::string m1;
+  int m2;
+
+  bool operator>(const BASE &b) const {
+    if (this->m2 != b.m2)
+      return this->m2 > b.m2;
+
+    return m1.compare(b.m2);
+  }
+};
+
+// static void BM_1(benchmark::State& state) {
+//   std::set<BASE> s;
+
+//   int idx = 0;
+
+//   for (auto _ : state) {
+//     BASE b;
+//     b.m1 = gen_random(12);
+//     b.m2 = ++idx;
+
+//     s.insert(b);
+//   }
+// }
+// BENCHMARK(BM_1);
+
+
+// static void BM_2(benchmark::State& state) {
+//   std::set<int> s;
+
+//   int idx = 0;
+
+//   for (auto _ : state) {
+//     s.insert(++idx);
+//   }
+// }
+// BENCHMARK(BM_2);
+// BENCHMARK_MAIN();
 
 int main(void) {
-  std::string channel_id = "012";
-  std::string zone_id = "003";
-  int32_t digital_id = 123456;
+  BASE b1;
+  b1.m1 = "a";
+  b1.m2 = 4;
 
-  std::stringstream ss(std::stringstream::in | std::stringstream::out);
-  ss << std::setw(3) << std::setfill('0') << channel_id;
-  ss << std::setw(3) << std::setfill('0') << zone_id;
-  ss << std::setw(6) << std::setfill('0') << digital_id;
+  BASE b2;
+  b2.m1 = "b";
+  b2.m2 = 3;
 
-  std::cout << ss.str() << std::endl;
+  BASE b3;
+  b3.m1 = "c";
+  b3.m2 = 2;
+
+  BASE b4;
+  b4.m1 = "d";
+  b4.m2 = 1;
+
+  std::set<BASE, std::greater<>> s;
+  s.insert(b1);
+  s.insert(b2);
+  s.insert(b3);
+  s.insert(b4);
+
+  for (const auto &e : s) {
+    std::cout << e.m1 << std::endl;
+  }
+
+  BASE b5;
+  b5.m1 = "e";
+  b5.m2 = 0;
+
+  s.insert(b5);
+
+  BASE b6;
+  b6.m1 = "f";
+  b6.m2 = -1;
+
+  s.insert(b6);
+  s.insert(b5);
 
   return EXIT_SUCCESS;
 }
